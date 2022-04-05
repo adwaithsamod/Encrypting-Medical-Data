75c04d057a7d0c17e16e905674ad86791b741d17
Adwaith Samod <adwaithsamod@gmail.com>
2022-04-06 02:28:31 +0530

    multiple file uploads

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+ 
+const app = express();
+ 
+const multer = require("multer");
+ 
+const path = require('path')
+ 
+const PORT = process.env.PORT || 5000;
+ 
+app.set("view engine", "ejs");
+ 
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+  },
+});
+ 
+var upload = multer({ storage: storage });
+ 
+var uploadMultiple = upload.fields([{ name: 'myImage', maxCount: 10 }])
+ 
+ 
+app.get("/", (req, res) => {
+  res.render("index");
+});
+ 
+app.post('/uploads', uploadMultiple, function (req, res, next) {
+ 
+    if(req.files){
+        // console.log(req.files)
+ 
+        // console.log("files uploaded")
+
+        res.render('index', {
+            msg: 'Files Uploaded!',
+            
+          });
+    }
+    
+})
+ 
+app.listen(PORT, () => {
+  console.log(`App is listening on Port ${PORT}`);
+});
